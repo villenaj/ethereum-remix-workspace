c91a5060752ccf1304bfc9cbfb52ac6afe6b4707
villenaj <villenajv43@gmail.com>
2026-01-09 15:26:22 +0800

    added FundMe contract

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,18 +4,56 @@ pragma solidity 0.8.31;
 
 import { PriceConverter } from "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
     address[] public funders;
     mapping(address funders => uint256 amount) public listOfFunders;
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
     
     function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough ETH");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough ETH");
         funders.push(msg.sender);
         listOfFunders[msg.sender] += msg.value;
     }
 
-    function withdraw() public {}
+    function withdraw() public onlyOwner {
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            listOfFunders[funder] = 0;
+        }
+        funders = new address[](0);
+
+        // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess,) = payable (msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner, "Not owner");
+        if (msg.sender != i_owner) revert NotOwner();
+        _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
 }
